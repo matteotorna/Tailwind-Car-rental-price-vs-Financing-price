4601e973a534dc4c112243134652126fbaf9813d
Matteo Tornaboni <108476185+matteotorna@users.noreply.github.com>
2023-09-28 22:41:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,29 +10,35 @@ document.addEventListener("DOMContentLoaded", function () {
         const maintenanceCost = parseFloat(document.getElementById("maintenanceCost").value) || 0;
         const tireCost = parseFloat(document.getElementById("tireCost").value) || 0;
         const usedCarValue = parseFloat(document.getElementById("usedCarValue").value) || 0;
+        const interestRateFinancing = parseFloat(document.getElementById("interestRateFinancing").value) || 0;
 
-        // Calcolo il costo totale del finanziamento auto
+        // Dichiarazione delle variabili come let invece di const
         let financingCost = insuranceCost + maintenanceCost + tireCost + autoServiceCost;
-        
-        // Sottrai il ricavo dalla vendita dell'usato (se opzione)
+        let leasingCost = serviceCost;
+
+        // Calcola l'importo dell'interesse per il finanziamento auto
+        const financingInterestAmount = (financingCost * (interestRateFinancing / 100));
+
+        // Aggiungi l'importo dell'interesse al costo totale del finanziamento auto
+        financingCost += financingInterestAmount;
+
+        // Sottrai il ricavo dalla vendita dell'usato (se opzione) solo per il finanziamento auto
         if (usedCarValue > 0) {
             financingCost -= usedCarValue;
         }
-
-        const leasingCost = serviceCost;
 
         const financingResult = document.getElementById("financingResult");
         const longTermRentalCost = document.getElementById("longTermRentalCost");
 
         if (financingCost < leasingCost) {
             financingResult.textContent = financingCost.toFixed(2); // Mostra il costo totale del finanziamento auto
-            longTermRentalCost.textContent = leasingCost.toFixed(2);
+            longTermRentalCost.textContent = leasingCost.toFixed(2); // Mostra il costo totale del noleggio a lungo termine
         } else if (leasingCost < financingCost) {
             financingResult.textContent = financingCost.toFixed(2); // Mostra il costo totale del finanziamento auto
-            longTermRentalCost.textContent = serviceCost.toString(); // Mostra il costo del servizio per il noleggio a lungo termine
+            longTermRentalCost.textContent = leasingCost.toFixed(2); // Mostra il costo totale del noleggio a lungo termine
         } else {
             financingResult.textContent = "Parità";
-            longTermRentalCost.textContent = serviceCost.toString(); // Mostra il costo del servizio per il noleggio a lungo termine
+            longTermRentalCost.textContent = leasingCost.toFixed(2); // Mostra il costo totale del noleggio a lungo termine
         }
     }
 });
